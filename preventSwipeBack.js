d155ff0f18c3fe45c52c75661f73af9c155c55ae
Phat Ho <phat@cloudkinetix.com>
2024-05-30 12:51:04 +0700

    add preventSwipeBack.js

---
+++ preventSwipeBack.js
@@ -0,0 +1,34 @@
+window.addEventListener("touchstart", handleTouchStart, false);
+window.addEventListener("touchmove", handleTouchMove, { passive: false });
+
+let xDown = null;
+let yDown = null;
+
+function getTouches(evt) {
+  return evt.touches || evt.originalEvent.touches;
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+  let xUp = evt.touches[0].clientX;
+  let yUp = evt.touches[0].clientY;
+
+  let xDiff = xDown - xUp;
+  let yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    evt.preventDefault();
+  }
+
+  // Reset values
+  xDown = null;
+  yDown = null;
+}
